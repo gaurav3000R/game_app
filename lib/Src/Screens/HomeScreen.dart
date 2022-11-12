2db8073f0c65815863fe491b6cf0b54e28ac0ecc
gaurav3000R <gauravrathva8@gmail.com>
2022-11-12 11:52:48 +0530

    FolderCreated

---
+++ lib/Src/Screens/HomeScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter first sDemo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Text('hiiii'),
+    );
+  }
+}
